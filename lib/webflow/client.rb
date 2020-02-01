f0001b177f4c9492daa524e8ec949a3bfce1c041
pbelasco <pbelasco@gmail.com>
2020-02-01 03:20:14 -0300

    logging instead of putting

--- lib/webflow/client.rb
+++ lib/webflow/client.rb
@@ -136,7 +136,7 @@ module Webflow
       result = JSON.parse(response.body)
       raise Webflow::Error.new(result) if response.code >= 400
       puts "------------------------result------------------------"
-      puts result
+      log.error result
       puts "------------------------result------------------------"
       result
       result
